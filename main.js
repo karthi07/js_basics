eb2e9e14bce4b009a99ff5fb0de9f216e8659dfa
sangeetha-ram <sangiammu1020@gmail.com>
2022-05-11 19:02:00 +0530

    created a function to generate random item from list

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 
-const todoUl = document.getElementById("todo-ul");
+var name_list = []
+
+function getRandom() {
+    var random_item =  Math.floor(Math.random() * name_list.length)
+    document.getElementById('selectedItem').innerHTML = name_list[random_item]
+}
 
 function addTask() {
     const todoText = document.getElementById("todo-text");
-
+    const todoUl = document.getElementById("todo-ul");
+    
     if (todoText.value.trim() === '') {
         alert('Enter some text');
     } else {
         const liTag = document.createElement('li');
         liTag.innerHTML = todoText.value.trim()
         todoUl.appendChild(liTag);
+        name_list.push(todoText.value.trim())
         todoText.value = ''
     }
 }
