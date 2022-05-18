24ca1ac58e8c2b015efb7e6c1cc61f10c2db1b17
sangi19 <sangiammu1020@gmail.com>
2022-05-18 13:50:08 +0530

    Add JSON file

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
+let  name_list = [ "pavi", "nandhini", "sangi"];
 
-const todoUl = document.getElementById("todo-ul");
+function getRandom() {
+    var random_item =  Math.floor(Math.random() * name_list.length)
+    document.getElementById('selectedItem').innerHTML = name_list[random_item]
+}
 
 function addTask() {
     const todoText = document.getElementById("todo-text");
-
+    const todoUl = document.getElementById("todo-ul");
+    
     if (todoText.value.trim() === '') {
         alert('Enter some text');
     } else {
         const liTag = document.createElement('li');
         liTag.innerHTML = todoText.value.trim()
         todoUl.appendChild(liTag);
+        name_list.push(todoText.value.trim())
         todoText.value = ''
     }
 }
-
+name_list =JSON.stringify(name_list);
+name_list=JSON.parse(name_list);
+console.log(name_list.addtsk);
