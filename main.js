b546e5227a78a364c101dfe44a3a654a5cb6dd4a
sangi19 <sangiammu1020@gmail.com>
2022-05-19 21:40:21 +0530

    Add data show fi=unction

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let  name_list = [];
+
+const todoText = document.getElementById("todo-text");
+const todoUl = document.getElementById("todo-ul");
+
 function getRandom() {
     var random_item =  Math.floor(Math.random() * name_list.length)
     document.getElementById('selectedItem').innerHTML = name_list[random_item]
@@ -6,12 +10,19 @@ function getRandom() {
 
 function getData(){
     fetch('users.json').then(response => response.json()).
-    then(data => name_list=data.name_list);
+    then(data => { name_list=data.name_list; showData() });
+  }
+
+ function showData(){
+    name_list.forEach(name=>{
+        const liTag = document.createElement('li');
+        liTag.innerHTML = name
+        todoUl.appendChild(liTag);
+    })
+
   }
 
 function addTask() {
-    const todoText = document.getElementById("todo-text");
-    const todoUl = document.getElementById("todo-ul");
     
     if (todoText.value.trim() === '') {
         alert('Enter some text');
