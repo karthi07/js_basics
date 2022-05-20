a3d4468ab0abe0541021350bc55bdd2b38b40daf
sangeetha-ram <sangiammu1020@gmail.com>
2022-05-20 19:22:32 +0530

    tasktypes add in addTasksFN

--- main.js
+++ main.js
@@ -1,38 +1,67 @@
-let  name_list = [];
+let  bf_list = [];
+let  lunch_list = [];
+let  dinner_list = [];
 
-const todoText = document.getElementById("todo-text");
-const todoUl = document.getElementById("todo-ul");
+const bfItemName = document.getElementById("bf-item-name");
+const bfNameList = document.getElementById("bf-name-list");
+const lunchItemName = document.getElementById("lunch-item-name");
+const lunchNameList = document.getElementById("lunch-name-list");
+const dinnerItemName = document.getElementById("dinner-item-name");
+const dinnerNameList = document.getElementById("dinner-name-list");
 
 function getRandom() {
     var random_item =  Math.floor(Math.random() * name_list.length)
     document.getElementById('selectedItem').innerHTML = name_list[random_item]
 }
 
-function getData(){
-    fetch('users.json').then(response => response.json()).
-    then(data => { name_list=data.name_list; showData() });
-  }
+// function getData(){
+//     fetch('users.json').then(response => response.json()).
+//     then(data => { name_list=data.name_list; showData() });
+//   }
 
- function showData(){
-    name_list.forEach(name=>{
+//  function showData(){
+//     name_list.forEach(name=>{
+//         const liTag = document.createElement('li');
+//         liTag.innerHTML = name
+//         todoUl.appendChild(liTag);
+//     })
+
+//   }
+
+function addTask(tasktype) {
+    if (tasktype == 1)
+    {
         const liTag = document.createElement('li');
-        liTag.innerHTML = name
-        todoUl.appendChild(liTag);
-    })
+        liTag.innerHTML = bfItemName.value.trim()
+        bfNameList.appendChild(liTag);
+        bf_list.push(bfItemName.value.trim())
+        bfItemName.value = ''
 
-  }
-
-function addTask() {
-    
-    if (todoText.value.trim() === '') {
-        alert('Enter some text');
-    } else {
-        const liTag = document.createElement('li');
-        liTag.innerHTML = todoText.value.trim()
-        todoUl.appendChild(liTag);
-        name_list.push(todoText.value.trim())
-        todoText.value = ''
     }
+
+    if (tasktype == 2)
+    {
+        const liTag = document.createElement('li');
+        liTag.innerHTML = lunchItemName.value.trim()
+        lunchNameList.appendChild(liTag);
+        lunch_list.push(lunchItemName.value.trim())
+        lunchItemName.value = ''
+
+    }
+
+
+    if (tasktype == 3)
+    {
+        const liTag = document.createElement('li');
+        liTag.innerHTML = dinnerItemName.value.trim()
+        dinnerNameList.appendChild(liTag);
+        dinner_list.push(dinnerItemName.value.trim())
+        dinnerItemName.value = ''
+
+    }
+
+
+    
 }
 
-getData()
+// getData()
